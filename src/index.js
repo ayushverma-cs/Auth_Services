d492a0541ccf6234b5cfd795909eb2ee86a76e57
Ayush <ayush.hstc@gmail.com>
2026-04-16 11:41:01 +0530

    Added get user by Id in repo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {PORT}= require('./config/serverConfig');
 const apiRoutes=require('./routes/index');
 // const {User}= require('./models/index');
 // const bcrypt = require('bcrypt');
+const  UserRepository = require('./repository/user-repository');
 const app = express();
 const prepareAndStartServer=()=>{
     app.use(bodyParser.json());
@@ -11,6 +12,9 @@ const prepareAndStartServer=()=>{
     app.use('/api',apiRoutes);
     app.listen(PORT,async ()=>{
         console.log(`Server is running on port ${PORT}`);
+        // const repo=new UserRepository();
+        // const response=await repo.getById(1);
+        // console.log(response);
         // const incomingpassword="1844 ";
         // const user=await User.findByPk(3);
         // const response = bcrypt.compareSync(incomingpassword,user.password);
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -22,5 +22,19 @@ class UserRepository{
             throw error;
         }
     }
+    async getById(userId){
+        try {
+            const user=await User.findByPk(userId,{
+                attributes:['email','id'],
+
+                
+            });
+            return user;
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw error;
+        }
+    }
+
 }
  module.exports=UserRepository;

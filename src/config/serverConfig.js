2bd5f0bcdddba4373db739c6d7dfdeae12b84462
Ayush <ayush.hstc@gmail.com>
2026-04-16 10:49:12 +0530

    Signup logic with password encryption done

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,6 +1,8 @@
 const dotenv= require('dotenv');
+const bcrypt = require('bcrypt');
 dotenv.config();
 module.exports={
-    PORT: process.env.PORT || 3001,
-    JWT_SECRET: process.env
+    PORT: process.env.PORT ,
+    SALT: bcrypt.genSaltSync(10)
+
 }
---
+++ src/repository/user-repository.js
@@ -0,0 +1,26 @@
+const {User}=require('../models/index');
+
+class UserRepository{
+    async create(data){
+        try {
+            const user=await User.create(data);
+            return user;
+            return user;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+    async destroy(userId){
+        try {
+            await User.destroy({
+                where:{
+                    id:userId
+                }
+            })
+        } catch (error) {            console.log("Something went wrong in repository layer");
+            throw error;
+        }
+    }
+}
+ module.exports=UserRepository;
